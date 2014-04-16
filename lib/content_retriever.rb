cddfa5bf6342b53ea4de69b49232dbe061af94bb
MindFy <shen.yafei55@gmail.com>
2014-04-16 09:43:57 +0800

    Update content_retriever.rb

--- lib/content_retriever.rb
+++ lib/content_retriever.rb
@@ -9,7 +9,7 @@ class ContentRetriver
 				title: '简单几步成为微信公众平台开发者',
 				description: '该公众账户是为了演示如何成为微信平台的开发者，如果想要了解更详细的内容，请点击该消息。',
 				picture: 'https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=3097029212&r=975188&token=1685872554',
-				url: 'http://linkedme.in/2013/09/25/turoial-to-be-wechat-public-account-developer/'}]
+				url: 'http://www.ahut.edu.cn/'}]
 		}
 	end
 end
